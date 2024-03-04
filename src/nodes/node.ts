b4a8b1fbeedf4d102776c31216a4495e214380f4
LouSarbe <louis.bessard@edu.devinci.fr>
2024-03-04 14:53:06 +0100

    Project is setup correctly ☀️

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,7 +1,14 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 import { BASE_NODE_PORT } from "../config";
 import { Value } from "../types";
+
+type NodeState = {
+  killed: boolean; // this is used to know if the node was stopped by the /stop route. It's important for the unit tests but not very relevant for the Ben-Or implementation
+  x: 0 | 1 | "?" | null; // the current consensus value
+  decided: boolean | null; // used to know if the node reached finality
+  k: number | null; // current step of the node
+};
 
 export async function node(
   nodeId: number, // the ID of the node
@@ -16,9 +23,28 @@ export async function node(
   node.use(express.json());
   node.use(bodyParser.json());
 
+  let state: NodeState = {
+    killed: false,
+    x: initialValue,
+    decided: false,
+    k: 0
+  };
+
+  // 1.
   // TODO implement this
   // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  node.get("/status", (req, res) => {
+    if (isFaulty) {
+      return res.status(500).send("faulty");
+    } else {
+      return res.status(200).send("live");
+    }
+  });
+
+  node.get("/getState", (req, res) => {
+    return res.json(state);
+  });
+
 
   // TODO implement this
   // this route allows the node to receive messages from other nodes
