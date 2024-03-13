1f4bad56272b8b98a8640ff60007fd8a3526b551
LouSarbe <louis.bessard@edu.devinci.fr>
2024-03-13 19:24:17 +0100

    Fetch issue left ⛅️

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { BASE_NODE_PORT } from "../config";
 import { Value, NodeState } from "../types";
+import { sendMessageToAll, consensusStep1, consensusStep2, areAllNodesDecided } from "../functions";
 
 export async function node(
   nodeId: number, // the ID of the node
@@ -29,7 +30,10 @@ export async function node(
     state.k = null;
   }
 
-  let receivedMessages: number[] = [];
+  let messagesStep1: Map<number, Value[]> = new Map();
+  let messagesStep2: Map<number, Value[]> = new Map();
+
+  // let receivedMessages: Value[] = [];
 
   // 1.
   // TODO implement this
@@ -45,18 +49,48 @@ export async function node(
   // TODO implement this
   // get the current state of a node
   node.get("/getState", (req, res) => {
-    return res.json(state);
+    res.status(200).json(state);
   });
 
   // TODO implement this
   // this route allows the node to receive messages from other nodes
   node.post("/message", (req: Request, res: Response) => {
-    if(isFaulty) {
+    if (isFaulty) {
       return res.status(500).send("faulty");
-    } else{
-    const message = req.body;
-    receivedMessages.push(message);
-    return res.send("success");
+    } else {
+      let { x, k, step } = req.body;
+
+      if (step === 1 && !state.decided && !state.killed) {
+        if (!messagesStep1.has(k)) {
+          messagesStep1.set(k, []);
+        }
+        messagesStep1.get(k)!.push(x);
+        let messages = messagesStep1.get(k)!;
+        if (messages.length >= N - F) {
+          state.x = consensusStep1(messages, state, N);
+          sendMessageToAll(2, nodeId, state, N);
+        }
+      }
+
+      if (step === 2 && !state.decided && !state.killed) {
+        if (!messagesStep2.has(k)) {
+          messagesStep2.set(k, []);
+        }
+        messagesStep2.get(k)!.push(x);
+        let messages = messagesStep2.get(k)!;
+        if (messages.length >= N - F) {
+          consensusStep2(messages, state, F, N);
+        }
+        sendMessageToAll(1, nodeId, state, N);
+        k = k + 1;
+      }
+
+      if (state.decided) {
+        if (!areAllNodesDecided(nodeId, N, F)) {
+          sendMessageToAll(step, nodeId, state, N);
+        }
+      }
+      return res.status(200).send("success");
     }
   });
 
@@ -67,7 +101,8 @@ export async function node(
       return res.status(500).send("not all nodes are ready");
     } else {
       if (!isFaulty) {
-        consensus();
+        state.k = 1;
+        sendMessageToAll(1, nodeId, state, N);
       }
       return res.status(200).send("success");
     }
@@ -77,129 +112,8 @@ export async function node(
   // this route is used to stop the consensus algorithm
   node.get("/stop", async (req, res) => {
     state.killed = true;
-    res.status(200).send("success");
+    res.status(200).send("killed");
   });
-
-  function processMessagesStep1() {
-    if (receivedMessages.length >= N - F) {
-      // Sufficient messages received
-      // Decide on a value based on received messages
-      let count0 = 0;
-      let count1 = 0;
-      for (const message of receivedMessages) {
-        if (message === 0) {
-          count0++;
-        } 
-        if (message === 1) {
-          count1++;
-        }
-      }
-      if (2 * count0 > N) {
-        return 0;
-      }
-      if (2 * count1 > N) {
-        return 1;
-      }
-      else {
-        return -1;
-      }
-    }
-    return null;
-  }
-
-  function processMessagesStep2() {
-    if (receivedMessages.length >= N - F) {
-      // Sufficient messages received
-      // Decide on a value based on received messages
-      let count0 = 0;
-      let count1 = 0;
-      for (const message of receivedMessages) {
-        if (message === 0) {
-          count0++;
-        } 
-        if (message === 1) {
-          count1++;
-        }
-      }
-      if (count0 > F) {
-        state.decided = true;
-        return 0;
-      }
-      if (count1 > F) {
-        state.decided = true;
-        return 1;
-      }
-      else {
-        if (count0 > 0)
-        {
-          return 0;
-        }
-        if (count1 > 0)
-        {
-          return 1;
-        }
-        else {
-          return Math.floor(Math.random() * 2);
-        }
-      }
-    }
-    return null;
-  }
-
-  // Function to initiate the consensus algorithm
-  function consensus() {
-    while(!state.decided && !state.killed) {
-      if (state.k != null){
-        state.k = state.k + 1;
-        receivedMessages = [];
-      }
-      for (let i = 0; i < N; i++) {
-        if (i !== nodeId) {
-          sendMessage(i);
-        }
-      }
-      let temp: any = null;
-      while (temp === null && !state.killed) {
-        temp = processMessagesStep1();
-      }
-      state.x = temp;
-      receivedMessages = [];
-      for (let i = 0; i < N; i++) {
-        if (i !== nodeId) {
-          sendMessage(i);
-        }
-      }
-      temp = null;
-      while (temp === null && !state.killed) {
-        temp = processMessagesStep2();
-      }
-      state.x = temp;
-    }
-  }
-
-function sendMessage(destinationNodeId: number) {
-  fetch(`http://localhost:${BASE_NODE_PORT + destinationNodeId}/message`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(state.x),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to send message');
-    }
-    return response.json();
-  })
-  /*.then(data => {
-    console.log('Message sent successfully');
-  })
-  .catch(error => {
-    console.error('Error sending message:', error);
-  })*/;
-}
-
-  
 
   // start the server
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
